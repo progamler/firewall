bd0ea4b469e54cc358ad432881178cb9c8e66002
progamler <github@guerilla-itde>
2014-02-06 11:04:16 +0100

    Update urls.py

--- firewallrequest/urls.py
+++ firewallrequest/urls.py
@@ -12,7 +12,6 @@ urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^show/rule/(?P<rule_id>\d+)/$', 'core.views.display_rule', name='show_rule'),
                        url(r'^create/host/$', 'core.views.create_host', name='create_host'),
-                       url(r'^create/rule/$', 'core.views.create_rule', name='create_rule'),
                        url(r'^save/rule/$', 'core.views.save_rule', name='save_rule'),
                        url(r'^list/rules/$', 'core.views.list_rules', name='list_rules')
 )
